0345a01d7a44587c3418bf76000d8e40d172617b
Nazemnykh Anton <nazemnykh.anton@gmail.com>
2015-01-27 09:35:43 +0500

    Рефакторинг ProcessManager: Сам ProcessManager только запускает определяемые в нём скрипты с настроенным в нём окржением. Директории из вне в конструкторе. SustainedProcess занимает поддержанием процесса во включенном/выключенном состоянии. NamedProcess позволяет задать имя процессу при создании.

--- processmanager.cpp
+++ processmanager.cpp
@@ -1,54 +1,94 @@
 #include "processmanager.h"
 
 #include <QTimer>
+#include <QDebug>
 
-ProcessManager::ProcessManager(QDir rootDir, bool running, QObject *parent) :
-    QObject (parent),
-    rootDir (rootDir),
-    running (running),
-    process (this)
+SustainedProcess::SustainedProcess(QString programName, QObject *parent)
+    : NamedProcess (programName, parent)
 {
-    QObject::connect (&process, SIGNAL(stateChanged(QProcess::ProcessState)), this, SLOT(onProcessStateChanged(QProcess::ProcessState)));
-
-    if (running)
-        start ();
+     QObject::connect (this, SIGNAL(stateChanged(QProcess::ProcessState)), this, SLOT(onProcessStateChanged(QProcess::ProcessState)));
+     time.restart();
 }
 
-void ProcessManager::keepRunning(bool run)
+void SustainedProcess::setSustainMode(SustainedProcess::SustainMode mode)
 {
-    if (running != run)
+    if (sustain != mode)
     {
-        running = run;
+        sustain = mode;
+        onProcessStateChanged(state());
+    }
+}
 
-        if (run)
-            start ();
-        else
-            stop ();
+SustainedProcess::SustainMode SustainedProcess::sustainMode() const
+{
+    return sustain;
+}
+
+void SustainedProcess::onProcessStateChanged(QProcess::ProcessState state)
+{
+    if ( sustain == KeepRunning && state == QProcess::NotRunning )
+        QTimer::singleShot (1000, this, SLOT(teststart()));
+
+    if ( sustain == KeepStopped && state != QProcess::NotRunning )
+        QTimer::singleShot (1000, this, SLOT(testkill()));
+}
+
+void SustainedProcess::teststart()
+{
+    qDebug() << "start" << time.elapsed();
+    start();
+}
+
+void SustainedProcess::testkill()
+{
+    qDebug() << "kill" << time.elapsed();
+    kill();
+}
+
+
+ProcessManager::ProcessManager(QDir binDir, QDir datDir, QDir tmpDir, QObject *parent) :
+    QObject (parent),
+    binDir (binDir), datDir (datDir), tmpDir (tmpDir),
+    startScript ("start", this), stopScript ("stop", this), deployScript ("deploy", this)
+{
+    QProcessEnvironment env = QProcessEnvironment::systemEnvironment();
+    env.insert("BIN", binDir.absolutePath());
+    env.insert("DAT", datDir.absolutePath());
+    env.insert("TMP", tmpDir.absolutePath());
+//    env.insert("PATH", env.value("PATH") + ":" + binDir.path());
+    startScript.setProcessEnvironment(env);
+
+    QProcess *scripts[3] = {&startScript, &stopScript, &deployScript};
+    for (int i = 0; i < 3; i ++)
+    {
+        scripts[i]->setWorkingDirectory(binDir.path ());
+        scripts[i]->setProcessEnvironment(env);
     }
 }
 
 void ProcessManager::start()
 {
-    process.setWorkingDirectory (rootDir.path ());
-    process.start ("start");
+    startScript.setSustainMode(SustainedProcess::KeepRunning);
 }
 
 void ProcessManager::stop()
 {
-    QProcess stopScript;
-    stopScript.setWorkingDirectory (rootDir.path ());
-    stopScript.start ("stop");
-    stopScript.waitForFinished (5000);
-    stopScript.kill ();
+    startScript.setSustainMode(SustainedProcess::Uncontrolled);
 
-    process.kill ();
+    stopScript.start();
+    if ( !stopScript.waitForFinished (5000) )
+        stopScript.kill ();
+    qDebug() << "stopScript killed";
+
+    startScript.setSustainMode(SustainedProcess::KeepStopped);
 }
 
-void ProcessManager::onProcessStateChanged(QProcess::ProcessState state)
+void ProcessManager::deploy(int versionFrom, int subversionFrom)
 {
-    if ( running && state == QProcess::NotRunning )
-        QTimer::singleShot (100, this, SLOT(start()));
+    QStringList arguments;
+    arguments << QString::number(versionFrom) << QString::number(subversionFrom);
 
-    if ( !running && state != QProcess::NotRunning )
-        QTimer::singleShot (100, this, SLOT(stop()));
+    deployScript.start(arguments);
+    if ( !deployScript.waitForFinished(30000) )
+        deployScript.kill();
 }
--- processmanager.h
+++ processmanager.h
@@ -4,26 +4,64 @@
 #include <QObject>
 #include <QDir>
 #include <QProcess>
+#include <QTime>
+
+class NamedProcess : public QProcess
+{
+    Q_OBJECT
+public:
+    explicit NamedProcess (QString programName, QObject *parent = 0)
+        : QProcess (parent), name (programName)
+    { }
+
+public slots:
+    void start () { QProcess::start(name); }
+    void start (const QStringList &arguments) { QProcess::start(name, arguments); }
+
+private:
+    QString name;
+};
+
+class SustainedProcess : public NamedProcess
+{
+    Q_OBJECT
+public:
+    explicit SustainedProcess (QString programName, QObject *parent = 0);
+
+    enum SustainMode {
+        Uncontrolled = 0,
+        KeepRunning = 1,
+        KeepStopped = 2
+    };
+    void setSustainMode (SustainMode mode);
+    SustainMode sustainMode () const;
+
+private slots:
+    void onProcessStateChanged (QProcess::ProcessState sustain);
+    void teststart();
+    void testkill();
+
+private:
+    SustainMode sustain;
+    QString name;
+    QTime time;
+};
 
 class ProcessManager : public QObject
 {
     Q_OBJECT
 public:
-    explicit ProcessManager(QDir rootDir = QDir::currentPath (), bool running = false, QObject *parent = 0);
+    explicit ProcessManager(QDir binDir, QDir datDir, QDir tmpDir, QObject *parent = 0);
     
 public slots:
-    void keepRunning (bool run);
-
-protected slots:
-    virtual void onProcessStateChanged (QProcess::ProcessState state);
-
-    virtual void start ();
-    virtual void stop ();
+    void start ();
+    void stop ();
+    void deploy (int versionFrom, int subversionFrom);
 
 private:
-    const QDir rootDir;
-    QProcess process;
-    bool running;
+    const QDir binDir, datDir, tmpDir;
+    SustainedProcess startScript;
+    NamedProcess stopScript, deployScript;
 };
 
 #endif // PROCESSMANAGER_H
